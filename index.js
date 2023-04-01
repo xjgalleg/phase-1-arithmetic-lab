b3a204e479d9231e999eb3ba6c07b7f9b44b914b
Josue Gallegos <gallegosjosue6@gmail.com>
2023-04-01 07:44:57 -0500

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 1
+let num2 = 62
+let multiply = function(num1, num2){
+    return num1 * num2;
+}
+multiply = (1, 62);
 
-// Write your code here
+let random = Math.floor(Math.random() + 0) + 1;
+
+let num3 = 16
+let num4 = 4
+let mod = function(num3, num4){
+    return num3 % num4;
+}
+mod = (16, 4);
+
+
+let max = Math.max(20);
